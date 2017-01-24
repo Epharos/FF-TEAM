1abb50594e2b720d6ed91bfdc142a4b19ac33464
Epharos <Neox30650@gmail.com>
2017-01-24 15:56:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+		<link rel="stylesheet" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+		<title>Fullfort - CS:GO</title>
+	</head>
+
+	<body>
+		<div class="w3-row" id="content">
+			<?php include('left.php'); ?>
+
+			<div class="w3-rest" id="webcontent">
+				<img class="w3-image" src="http://fullfort.net/wp-content/uploads/2016/09/Untitled.gif">
+
+				<h1>Bienvenue sur le site officiel de la team FULLFORT !</h1>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ left.php
@@ -0,0 +1,22 @@
+<div id="left-part" class="w3-container w3-col w3-left">
+	<header>
+		<img alt="logo" style="width : 150px; height : 150px;" src="http://fullfort.net/wp-content/uploads/2016/08/FULLFORT-WHITE-1K-150x150.png">
+	</header>
+
+	<nav>
+		<hr id="alogo">
+
+		<form action="research.php">
+			<input type="text" placeholder="Rechercher ...">
+		</form>
+
+		<hr id="asearch">
+
+		<ul>
+			<a href="index.php"><li>ACCUEIL</li></a>
+			<a href="members.php"><li>MEMBRES</li></a>
+			<a href="compet.php"><li>COMPETITIONS</li></a>
+			<a href="join.php"><li>NOUS REJOINDRE</li></a>
+		</ul>
+	</nav>
+</div>
